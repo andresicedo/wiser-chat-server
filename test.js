d4689fbb40b2fa979dc45cb20e6552b4ec26ced6
Andres Icedo <andresicedo24@gmail.com>
2021-04-16 14:46:58 -0500

    updated index js

--- test.js
+++ test.js
@@ -1,30 +1,31 @@
-/**
- * TODO(developer): Uncomment the following line before running the sample.
- */
-// const projectId = 'YOUR_PROJECT_ID';
+// /**
+//  * TODO(developer): Uncomment the following line before running the sample.
+//  */
+// // const projectId = 'YOUR_PROJECT_ID';
 
-// Imports the Google Cloud client library
-const {Translate} = require('@google-cloud/translate').v2;
-require('dotenv').config();
+// // Imports the Google Cloud client library
+// const {Translate} = require('@google-cloud/translate').v2;
+// require('dotenv').config();
 
-const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
-// Instantiates a client
-const translate = new Translate({
-    credentials: CREDENTIALS,
-    projectId: CREDENTIALS.project_id
-});
+// const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
+// // Instantiates a client
+// const translate = new Translate({
+//     credentials: CREDENTIALS,
+//     projectId: CREDENTIALS.project_id
+// });
 
-async function quickStart() {
-  // The text to translate
-  const text = 'Hello, world!';
+// async function quickStart() {
+//   // The text to translate
+//   const text = 'Hello, world!';
 
-  // The target language
-  const target = 'es';
+//   // The target language
+//   const target = 'es';
 
-  // Translates some text into Russian
-  const [translation] = await translate.translate(text, target);
-  console.log(`Text: ${text}`);
-  console.log(`Translation: ${translation}`);
-}
+//   // Translates some text into Russian
+//   const [translation] = await translate.translate(text, target);
+//   console.log(`Text: ${text}`);
+//   console.log(`Translation: ${translation}`);
+// }
 
-quickStart();
+// quickStart();
+
