3a7a874f3a0359b9221e25f8f97ff8fe3e6cbb89
Andres Icedo <andresicedo24@gmail.com>
2021-04-16 20:58:12 -0500

    updated indexjs

--- index.js
+++ index.js
@@ -5,14 +5,8 @@ const cors = require('cors');
 const {Translate} = require('@google-cloud/translate').v2;
 require('dotenv').config();
 const CREDENTIALS = JSON.parse(process.env.CREDENTIALS);
-
-const translate = new Translate({
-  credentials: CREDENTIALS,
-  projectId: CREDENTIALS.project_id
-});
-
+const translate = new Translate({credentials: CREDENTIALS,projectId: CREDENTIALS.project_id});
 const { addUser, removeUser, getUser, getUsersInRoom } = require('./users');
-
 const router = require('./router');
 
 const app = express();
@@ -25,38 +19,36 @@ app.use(router);
 io.on('connect', (socket) => {
   socket.on('join', ({ name, room }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
-
     if(error) return callback(error);
-
     socket.join(user.room);
-
     socket.emit('message', { user: 'admin', text: `${user.name}, welcome to the ${user.room} chat`});
     socket.broadcast.to(user.room).emit('message', { user: 'admin', text: `${user.name} has entered the chat` });
-
     io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
-
     callback();
   });
 
   socket.on('sendMessage', (message, callback) => {
     const user = getUser(socket.id);
-    // try {
-      async function quickStart() {
-        // The text to translate
-        const text = message;
-        // The target language
-        const target = 'es';
-        // Translates some text into Spanish
-        const [translation] = await translate.translate(text, target);
-        io.to(user.room).emit('message', { user: user.name, text: message, translation: translation});
-      }
-      quickStart();
-    // }
-    // catch (error) {
-    //   console.log(error)
-    // }
+    io.to(user.room).emit('message', { user: user.name, text: message });
     callback();
   });
+
+  async function quickStart() {
+    // The text to translate
+    const text = message;
+    // The target language
+    const target = 'es';
+    // Translates some text into Spanish
+    const [translation] = await translate.translate(text, target);
+    return translation;
+  }
+
+  socket.on('sendTranslation', (translation, callback) => {
+    const user = getUser(socket.id);
+    io.to(user.room).emit('translation', { user: user.name, translation });
+    callback();
+  });
+
 
   socket.on('disconnect', () => {
     const user = removeUser(socket.id);
