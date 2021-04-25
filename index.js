ba1575cbe14ccb5e2d872be967f1eafe25034517
Andres Icedo <andresicedo24@gmail.com>
2021-04-24 21:09:34 -0500

    updated send message socket emit index js

--- index.js
+++ index.js
@@ -16,8 +16,123 @@ const io = socketio(server);
 app.use(cors());
 app.use(router);
 
+
+let languagesDictionary = {
+  "Select A Language": "en",
+  "English": "en",
+  "Spanish": "es",
+  "Albanian": "sq",
+  "Amharic": "am",
+  "Arabic": "ar",
+  "Afrikaans": "af",
+  "Armenian": "hy",
+  "Azerbaijani": "az",
+  "Basque": "eu",
+  "Belarusian": "be",
+  "Bengali": "bn",
+  "Bosnian": "bs",
+  "Bulgarian": "bg",
+  "Catalan": "ca",
+  "Cebuano": "ceb",
+  "Chinese (Simplified)": "zh-CN",
+  "Chinese (Traditional)": "zh-TW",
+  "Corsican": "co",
+  "Croatian": "hr",
+  "Czech": "cs",
+  "Danish": "da",
+  "Dutch": "nl",
+  "Esperanto": "eo",
+  "Estonian": "et",
+  "Finnish": "fi",
+  "French": "fr",
+  "Frisian": "fy",
+  "Galician": "gl",
+  "Georgian": "ka",
+  "German": "de",
+  "Greek": "el",
+  "Gujarati": "gu",
+  "Haitian Creole": "ht",
+  "Hausa": "ha",
+  "Hawaiian": "haw",
+  "Hebrew_he": "he",
+  "Hebrew iw": "iw",
+  "Hindi": "hi",
+  "Hmong": "hmn",
+  "Hungarian": "hu",
+  "Icelandic": "is",
+  "Igbo": "ig",
+  "Indonesian": "id",
+  "Irish": "ga",
+  "Italian": "it",
+  "Japanese": "ja",
+  "Javanese": "jv",
+  "Kannada": "kn",
+  "Kazakh": "kk",
+  "Khmer": "km",
+  "Kinyarwanda": "rw",
+  "Korean": "ko",
+  "Kurdish": "ku",
+  "Kyrgyz": "ky",
+  "Lao": "lo",
+  "Latin": "la",
+  "Latvian": "lv",
+  "Lithuanian": "lt",
+  "Luxembourgish": "lb",
+  "Macedonian": "mk",
+  "Malagasy": "mg",
+  "Malay": "ms",
+  "Malayalam": "ml",
+  "Maltese": "mt",
+  "Maori": "mi",
+  "Marathi": "mr",
+  "Mongolian": "mn",
+  "Myanmar (Burmese)": "my",
+  "Nepali": "ne",
+  "Norwegian": "no",
+  "Nyanja (Chichewa)": "ny",
+  "Odia (Oriya)": "or",
+  "Pashto": "ps",
+  "Persian": "fa",
+  "Polish": "pl",
+  "Portuguese (Portugal, Brazil)": "pt",
+  "Punjabi": "pa",
+  "Romanian": "ro",
+  "Russian": "ru",
+  "Samoan": "sm",
+  "Scots Gaelic": "gd",
+  "Serbian": "sr",
+  "Sesotho": "st",
+  "Shona": "sn",
+  "Sindhi": "sd",
+  "Sinhala (Sinhalese)": "si",
+  "Slovak": "sk",
+  "Slovenian": "sl",
+  "Somali": "so",
+  "Sundanese": "su",
+  "Swahili": "sw",
+  "Swedish": "sv",
+  "Tagalog (Filipino)": "tl",
+  "Tajik": "tg",
+  "Tamil": "ta",
+  "Tatar": "tt",
+  "Telugu": "te",
+  "Thai": "th",
+  "Turkish": "tr",
+  "Turkmen": "tk",
+  "Ukrainian": "uk",
+  "Urdu": "ur",
+  "Uyghur": "ug",
+  "Uzbek": "uz",
+  "Vietnamese": "vi",
+  "Welsh": "cy",
+  "Xhosa": "xh",
+  "Yiddish": "yi",
+  "Yoruba": "yo",
+  "Zulu": "zu"
+}
+
 io.on('connect', (socket) => {
-  socket.on('join', ({ name, room }, callback) => {
+  socket.on('join', ({ name, room, chosenLanguage }, callback) => {
     const { error, user } = addUser({ id: socket.id, name, room });
     if (error) return callback(error);
     socket.join(user.room);
@@ -27,13 +142,12 @@ io.on('connect', (socket) => {
     callback();
   });
 
-  socket.on('sendMessage', async (message, callback) => {
+  socket.on('sendMessage', async ({message, chosenLanguage}, callback) => {
     const user = getUser(socket.id);
     try {
       const text = message;
-      const target = 'es';
+      const target = languagesDictionary[chosenLanguage];
       const [translation] = await translate.translate(text, target);
-      console.log(translation)
       io.to(user.room).emit('message', { user: user.name, text: message, translation });
 
     } catch (error) {
